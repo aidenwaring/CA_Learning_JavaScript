dcf04867b8b39619b54847b15af3b0923086f79a
aidenwaring <aiden.waring@icloud.com>
2020-05-25 21:51:28 +1000

    Initial commit. Demo JavaScript test files

---
+++ rubyvsjs_0.js
@@ -0,0 +1,13 @@
+let user = "tom"
+let age = "18"
+if (user == "tom"){
+  console.log("Hello tom")
+} else {
+  console.log("Hello someone else other than tom")
+}
+let input = prompt("What age are you?", "Age Tester")
+if (age >= 18){
+  console.log("Welcome to the casino!")
+} else {
+  console.log("You have been denied entry.")
+}
